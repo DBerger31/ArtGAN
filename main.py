5531d1496f9cb56b53140da69ac3366862ab8c5e
Daniel Berger <DanielBergerNY@gmail.com>
2022-07-13 14:06:45 -0400

    layers

--- main.py
+++ main.py
@@ -3,15 +3,27 @@ from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, Flatten
 import matplotlib.pyplot as plt
-
+import numpy as np
 # Testing Dataset
 from keras.datasets import mnist
-#download mnist data and split into train and test sets
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-plt.imshow(X_train[0])
-plt.show()
+# Constants and hyperparameters
+batch_size = 64
+num_of_channels = 1
+num_classes = 10
+image_size = 28
+latent_dim = 128
+
+#download mnist data and split into train and test sets
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+all_digits = np.concatenate([x_train, x_test])
+all_labels = np.concatenate([y_train, y_test])
 
 class Discriminator(keras.Sequential):
     def __init__(self):
-        keras.layers.InputLayer(28,28)
+        self.conv1 = tf.keras.layers.Conv2D()
+        self.conv2 = tf.keras.layers.Conv2D()
+        self.connection = tf.keras.layers.Flatten()
+        self.dense = tf.keras.layers.Dense()
+
+
