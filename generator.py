b851cf10238fc34f333193a2d1534885bbf56de3
amy-tse <amytse0215@gmail.com>
2022-07-13 17:36:19 -0400

    generator
    rough draft of generator using CNN

---
+++ generator.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+from tensorflow.keras.layers import Input, Reshape, Dropout, Dense 
+from tensorflow.keras.layers import Flatten, BatchNormalization
+from tensorflow.keras.layers import Activation, ZeroPadding2D
+from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.layers import UpSampling2D, Conv2D
+from tensorflow.keras.models import Sequential, Model, load_model
+from tensorflow.keras.optimizers import Adam
+import numpy as np
+
+# Generated resolution factor 
+generator_res = 2 
+# (1=32, 2=64, 3=96, 4=128, etc.)
+
+generator_sq = 32 * generator_res # rows/cols
+
+img_channels = 3
+
+
+# Size of vector to generate images from
+seed_size = 100
+
+
+def build_generator(seed_size, channels):
+    model = Sequential()
+
+    model.add(Dense(4*4*256,activation="relu",input_dim=seed_size))
+    model.add(Reshape((4,4,256)))
+
+    model.add(UpSampling2D())
+    model.add(Conv2D(256,kernel_size=3,padding="same"))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Activation("relu"))
+
+    model.add(UpSampling2D())
+    model.add(Conv2D(256,kernel_size=3,padding="same"))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Activation("relu"))
+   
+  
+    model.add(UpSampling2D())
+    model.add(Conv2D(128,kernel_size=3,padding="same"))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Activation("relu"))
+
+    if generator_res>1:
+      model.add(UpSampling2D(size=(generator_res,generator_res)))
+      model.add(Conv2D(128,kernel_size=3,padding="same"))
+      model.add(BatchNormalization(momentum=0.8))
+      model.add(Activation("relu"))
+
+    # Final CNN layer
+    model.add(Conv2D(channels,kernel_size=3,padding="same"))
+    model.add(Activation("tanh"))
+
+    return model
+
+
+generator = build_generator(seed_size, img_channels)
+
+noise = tf.random.normal([1, seed_size])
+generated_image = generator(noise)
+
+plt.imshow(generated_image[0, :, :, 0])
+plt.show()
