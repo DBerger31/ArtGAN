596731989adb141b07d8d72f7c4f34eba7db8328
Daniel Berger <DanielBergerNY@gmail.com>
2022-07-13 17:02:43 -0400

    compile disc, train

--- main.py
+++ main.py
@@ -19,11 +19,41 @@ latent_dim = 128
 all_digits = np.concatenate([x_train, x_test])
 all_labels = np.concatenate([y_train, y_test])
 
-class Discriminator(keras.Sequential):
-    def __init__(self):
-        self.conv1 = tf.keras.layers.Conv2D()
-        self.conv2 = tf.keras.layers.Conv2D()
-        self.connection = tf.keras.layers.Flatten()
-        self.dense = tf.keras.layers.Dense()
+#reshape data to fit model
+X_train = x_train.reshape(60000,28,28,1)
+X_test = x_test.reshape(10000,28,28,1)
 
+from keras.utils import to_categorical
+#one-hot encode target column
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+class Discriminator():
+    def __init__(self):
+        # super(self).__init__
+        self.model = keras.Sequential([
+            Conv2D(64, kernel_size=3, activation='relu', input_shape=(28,28,1)),
+            Conv2D(32, kernel_size=3, activation='relu'),
+            Flatten(),
+            Dense(10, activation='softmax')]
+        )
+        # self.conv1 = tf.keras.layers.Conv2D(64, kernel_size=3, activation='relu', input_shape=(28,28,1))
+        # self.conv2 = tf.keras.layers.Conv2D(32, kernel_size=3, activation='relu')
+        # self.connection = tf.keras.layers.Flatten()
+        # self.dense = tf.keras.layers.Dense(10, activation='softmax')
+        # self.opt = keras.optimizers.Adam(learning_rate=0.01)
+
+    def compile_discriminator(self):
+        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    def train(self):
+        self.model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=3)
+
+cnn = Discriminator()
+cnn.compile_discriminator()
+cnn.train()
+
+
+#train the model
+# cnn.fit(x_train, y_train, validation_data=(x_test, y_test), epochs =3)
 
