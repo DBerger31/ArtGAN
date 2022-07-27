ad4b1f72d6504b36b225eb26a5478057956bc14b
Daniel Berger <DanielBergerNY@gmail.com>
2022-07-27 12:02:23 -0400

    resize

--- resize.py
+++ resize.py
@@ -1,21 +1,12 @@
+import PIL
+import os
+import os.path
 from PIL import Image
-import os, sys
 
-path = ('C:/Users/Danie/Documents/ArtGAN/train')
-
-def resize():
-    for item in os.listdir(path):
-        if os.path.isfile(item):
-            im = Image.open(item)
-            f, e = os.path.splitext(item)
-            imResize = im.resize((64,64), Image.ANTIALIAS)
-            imResize.save('/trainre'+ f + '.jpg')
-
-resize()
-
-# from PIL import Image
-# img = Image.open('/your iamge path/image.jpg') # image extension *.png,*.jpg
-# new_width  = 128
-# new_height = 128
-# img = img.resize((new_width, new_height), Image.ANTIALIAS)
-# img.save('/new directory path/output image name.png')
+f = ('C:/Users/Danie/Downloads/train/train')
+newf = ('C:/Users/Danie/Documents/ArtGAN/trainre')
+for file in os.listdir(f):
+    f_img = newf+"/"+file
+    img = Image.open(f_img)
+    img = img.resize((64,64))
+    img.save(f_img)
