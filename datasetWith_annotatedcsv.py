329146fda4e04569e639f117e492146eafc62f7d
anthony-b1 <anthony78510@gmail.com>
2022-07-27 13:24:10 -0400

    Create datasetWith_annotatedcsv.py

---
+++ datasetWith_annotatedcsv.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+df = pd.read_csv('trainnew.csv')
+file_paths = df['filename'].values
+labels = df['genre'].values
+ds_train = tf.data.Dataset.from_tensor_slices((file_paths, labels))
+
+def read_image(image_file, label):
+    image = tf.io.read_file(r"top10_train" + "/" + image_file)
+    image = tf.image.decode_image(image, channels=3, dtype=tf.float32)
+    return image, label
+
+ds_train = ds_train.map(read_image)
+
+for x, y in ds_train:
+    print(x)
