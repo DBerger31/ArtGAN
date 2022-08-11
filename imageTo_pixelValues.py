39736e2653c4b6b5919c635f1bc97e27654d3cb7
anthony-b1 <anthony78510@gmail.com>
2022-08-11 18:08:06 -0400

    Get images.npy and labels.txt (Update)

--- imageTo_pixelValues.py
+++ imageTo_pixelValues.py
@@ -18,57 +18,54 @@ PIL.Image.MAX_IMAGE_PIXELS = 933120000
 
 # Making images.npy
 # Convert each image to pixel values and put it in an array
-
-f = r'top10_train'
+r"""
+f = r'5_Genres'
 array = []
 for file in os.listdir(f):
     f_img = f+"/"+file
     img = Image.open(f_img)
     array.append(np.array(img))
-
 array = np.stack(array, axis = 0)
 
-# https://stackoverflow.com/questions/28439701/how-to-save-and-load-numpy-array-data-properly
 # Save 4D array to a npy file
-np.save('images.npy', array)
+np.save('images(128x128).npy', array)
 
 # Double check whether the first element in the numpy array in images.npy is equal to the first image numpy array
-d = np.load('images.npy')
+d = np.load('images(128x128).npy')
 print(d.shape)
 print(d[0])
-img = Image.open('10.jpg')
-x = np.array(img)
-print("10.jpg matrix")
-print(x)
-print(d[0] == x)
-
+"""
+######################################################################################
 # Making labels.txt
 # Match filename with it's correct labels and put all labels to an array
 # Then save it as txt
-r"""
-f = r'top10_train'
-array = []
+
+f = r'5_Genres'
 filename = []
 for file in os.listdir(f):
     filename.append(file)
 
-df = pd.read_csv(r'trainnew.csv')
+df = pd.read_csv(r'train_info.csv')
 label = []
 for name in filename:
     x = df.loc[df['filename'] == name, 'genre'].iloc[0]
     label.append(x)
+print(label)
+
+# label each genre with a number
+label = [0 if i =='abstract' 
+        else 1 if i == 'landscape'
+        else 2 if i =='religious painting'
+        else 3 if i =='portrait'
+        else 4 if i =='sketch and study'
+        else i for i in label]
+print(label)
 
 # Save 1D array to a txt file
-np.savetxt('labels.txt', label, fmt='%d')
+np.savetxt('5_Genres_Labels.txt', label, fmt='%d')
 
-f = r'top10_train'
-filename = []
-for file in os.listdir(f):
-    filename.append(file)
-
-d = np.loadtxt('labels.txt', dtype=int)
+d = np.loadtxt('5_Genres_Labels.txt', dtype=int)
 print(d.shape)
-"""
 
 # Print program runtime
 elapsed = (time.time() - start_time)
