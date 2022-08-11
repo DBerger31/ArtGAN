12cd1761eee881e82ec100965e96761e7a114083
Daniel Berger <DanielBergerNY@gmail.com>
2022-08-10 22:25:37 -0400

    padding to each row

--- GAN/static/main.js
+++ GAN/static/main.js
@@ -11,15 +11,13 @@ function show_image(src, width, height, alt) {
 function show_multi_image(width, height, alt) {
     var row = document.createElement("div");
     row.classList.add('row')
-    row.style.cssText =  'display: grid; grid-auto-flow: column; gap: 1px; align-items: center; justify-items: center;';
+    row.style.cssText =  'display: grid; grid-auto-flow: column; gap: 1px; align-items: center; justify-items: center; padding-bottom: 10px';
     row.id = 'row-imgs'
     // document.body.appendChild(row)
     for(var i=0; i <10; i++){
 
         var col = document.createElement("div");
         col.id = 'col-md-8'
-        // col.style.cssText =  'display: grid; grid-auto-flow: column; gap: 4px; align-items: center; justify-items: center;';
-
         var img = document.createElement("img");
         img.src = '/static/img' + i + '.png?' + new Date().getTime();
         img.width = width;
